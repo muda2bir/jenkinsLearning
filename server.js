4ec8b2bf6a6c31497b439c7c18a7f853d20af7f7
Mudabbir Hussain Momin <mudabbir.hm@gmail.com>
2022-12-31 13:39:36 +0530

    changed the port

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
 	if (req.url === "/") {
-		res.end("Home Page");
+		res.end("<h1>Home Page</h1>");
 	}
 });
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 7860 
 server.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
 });
